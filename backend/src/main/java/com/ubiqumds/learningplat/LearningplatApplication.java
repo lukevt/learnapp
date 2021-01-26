0a3332d584fd7d364f11dafe8171f82e4327d784
lfcsvt <lfcsvt@gmail.com>
2021-01-26 10:59:22 +0000

    project created

---
+++ backend/src/main/java/com/ubiqumds/learningplat/LearningplatApplication.java
@@ -0,0 +1,13 @@
+package com.ubiqumds.learningplat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningplatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningplatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ubiqumds/learningplat/LearningplatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubiqumds.learningplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningplatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
