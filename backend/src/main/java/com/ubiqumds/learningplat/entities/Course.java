5aee3c0875c24a2db3c4c4b5b746c147ef1cbb95
lfcsvt <lfcsvt@gmail.com>
2021-02-27 10:56:43 +0000

    offer entity implemented

--- backend/src/main/java/com/ubiqumds/learningplat/entities/Course.java
+++ backend/src/main/java/com/ubiqumds/learningplat/entities/Course.java
@@ -1,12 +1,17 @@
 package com.ubiqumds.learningplat.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name= "tb_course")
@@ -20,6 +25,8 @@ public class Course implements Serializable{
 	private  String imgUri;
 	private  String imgGrayUri;
 	
+	@OneToMany(mappedBy ="course")
+	private List <Offer> offers = new ArrayList<>();
 	public Course() {
 		
 	}
@@ -62,6 +69,12 @@ public class Course implements Serializable{
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+	
+	
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
