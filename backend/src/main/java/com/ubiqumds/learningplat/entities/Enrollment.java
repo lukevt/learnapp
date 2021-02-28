54fce4b2ef3ac5f339c560f021137ec12e422a85
lfcsvt <lfcsvt@gmail.com>
2021-02-28 10:34:41 +0000

    enrollment and enrollmentpk entities implemented

---
+++ backend/src/main/java/com/ubiqumds/learningplat/entities/Enrollment.java
@@ -0,0 +1,116 @@
+package com.ubiqumds.learningplat.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.ubiqumds.learningplat.entities.pk.EnrollmentPK;
+
+@Entity
+@Table(name="tb_enrollment")
+public class Enrollment {
+	
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
+	
+	@Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	private boolean available;
+	private boolean onlyUpdate;
+	
+	public Enrollment() {
+		
+	}
+
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+			boolean onlyUpdate) {
+		super();
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.available = available;
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudent(User user) {
+		id.setUser(user);
+	}
+	
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enrollment other = (Enrollment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
